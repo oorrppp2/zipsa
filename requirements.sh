ba8042aa9a12a2a4c53a96a72e793ec6f2446796
jschoi <cjs0818@ust.ac.kr>
2021-03-21 20:44:59 +0900

    requirements.sh added

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+sudo apt install -y ros-$ROS_DISTRO-py-trees-ros
+sudo apt install -y ros-$ROS_DISTRO-rosbridge-server
+sudo apt install -y ros-$ROS_DISTRO-twist-mux
+
+#cd ~/catkin_ws/src
+#git clone https://github.com/byeongkyu/dynamixel_ros_control.git
